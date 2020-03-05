97f80f919371db731c39a56678d610663e643737
namelessx <stanescuvladut10@gmail.com>
2020-03-05 20:19:13 +0200

    Error handler v3

--- index.js
+++ index.js
@@ -238,11 +238,9 @@ client.on('message',message => {
     } else if(message.content.startsWith(`until it's done done`)){
         message.channel.send("until the sun comes up in the morn")
     }
+    catch (err, message){
+        catchErr (err);
+    }
 })
-catch(err){
-    catchErr (err,message);
-}
-
-
 
 client.login(token);
